77ef89b19b3b680761eb2575f337525020aa05f9
kithon-sd <123kithon@gmail.com>
2019-11-29 14:24:23 +0400

    changed initial route, added search for single album

--- router.js
+++ router.js
@@ -6,7 +6,29 @@ const url = 'http://ws.audioscrobbler.com/2.0/'
 
 
 router.get('/', async (req, res) => {
-    const response = await axios.get(`${url}?method=album.search&album=${req.query.query}&api_key=${config.API_KEY}&format=json`)
+    const response = await axios.get(`${url}`, {
+        params: {
+            method: 'album.search',
+            album: req.query.query,
+            api_key: config.API_KEY,
+            page: req.query.page || 1,
+            limit: req.query.limit || 10,
+            format: 'json'
+        }
+    })
+    res.json(response.data)
+})
+
+router.get('/placeholder', async (req, res) => {
+    const response = await axios.get(`${url}`, {
+        params: {
+            method: 'album.getInfo',
+            api_key: config.API_KEY,
+            format: 'json',
+            artist: req.query.artist,
+            album: req.query.album
+        }
+    })
     res.json(response.data)
 })
 
